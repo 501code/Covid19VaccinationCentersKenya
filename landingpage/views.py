41f0eb0db6c23009bb9e91da6a7bfef69ac706ac
Nick Hargreaves <hargreaves1234@gmail.com>
2021-05-18 23:34:17 +0300

    fix map centering

--- landingpage/views.py
+++ landingpage/views.py
@@ -23,8 +23,7 @@ class LandingPage(View):
             loc1 = loc.split(",")[0].strip()
             loc2 = loc.split(",")[1].strip()
             locs.append({'name': place.name, 'loc1': loc1, 'loc2': loc2})
-        center = request.POST.get('center', {"lat": locs[0]['loc1'], "lng": locs[0]['loc2']})
-        center = "0,0"
+        center = request.POST.get('center', '{lat: '+locs[0]["loc1"]+', lng: '+locs[0]["loc2"]+'}')
         context = {
             'GOOGLE_MAPS_API_KEY': settings.GOOGLE_MAPS_API_KEY,
             'places': locs,
