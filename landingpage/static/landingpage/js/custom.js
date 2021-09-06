28a3040ec44fe8dda656c59a3cb91a1ce8c2c4a3
Maxwell Mandela <mxmandela@gmail.com>
2021-09-06 19:32:23 +0300

    update template faqs

--- landingpage/static/landingpage/js/custom.js
+++ landingpage/static/landingpage/js/custom.js
@@ -3,12 +3,16 @@ let list = document.getElementsByClassName("faq-list")[0]
 fetch("/static/data/faqs.json").then(res => {
     res.json().then(items => {
         items.forEach((element, index) => {
+
+            // add the question
             let li = document.createElement("li")
             li.setAttribute("data-aos", "fade-up")
             li.innerHTML = `
-                <i class="bx bx-help-circle icon-help"></i> 
+                 
                 <a data-toggle="collapse" class="collapse" href="#faq-list-${index + 1}">
-                    ${element.q} 
+                    
+                <i class="bx bx-help-circle iicon-help"></i>
+                ${element.q} 
                     <i class="bx bx-chevron-down icon-show"></i>
                     <i class="bx bx-chevron-up icon-close"></i>
                 </a>
@@ -19,6 +23,7 @@ fetch("/static/data/faqs.json").then(res => {
 
             list.appendChild(li)
 
+            // add the answer
             let html = document.getElementById(`faq-list-${index + 1}`)
             let paragraphs = element.a.split("\n")
 
@@ -27,8 +32,6 @@ fetch("/static/data/faqs.json").then(res => {
                 ell.innerText = i
                 html.appendChild(ell)
             })
-
-            // html.appendChild(li)
         });
     })
 })
