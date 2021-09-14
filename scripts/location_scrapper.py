8e9234e70edbc6a5059d766886080dc85bcba3d2
Nick Hargreaves <hargreaves1234@gmail.com>
2021-09-14 21:34:35 +0300

    scrape facilities' locations

--- scripts/location_scrapper.py
+++ scripts/location_scrapper.py
@@ -1,9 +1,37 @@
 import requests
-from api.models import SubCounty
+from api.models import SubCounty, VaccineCenter
 
 
 def run():
     scrape_subcounties()
+    scrape_hospitals()
+
+
+def scrape_hospitals():
+    google_api_key = ''
+    from api.models import SubCounty
+    subcounties = SubCounty.objects.all()
+    for sub_county in subcounties:
+        facilities = VaccineCenter.objects.filter(sub_county=sub_county, location__isnull=True)
+        places_search_path = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?radius=50000' \
+                             '&type=hospital&key=' + google_api_key
+        for facility in facilities:
+            url = places_search_path + "&name=" + facility.name + "&location=" + sub_county.location
+            response = requests.get(url).json()
+            results = response['results']
+            if len(results) < 1:
+                # try text search
+                url = "https://maps.googleapis.com/maps/api/place/textsearch/json?key=" + google_api_key \
+                      + "&query=" + facility.name
+                response = requests.get(url).json()
+                results = response['results']
+            if len(results) > 0:
+                place = results[0]
+                location = str(place['geometry']['location']['lat']) + ',' + str(place['geometry']['location']['lng'])
+                facility.location = location
+                facility.save()
+            else:
+                print("404 - ", sub_county.name, str(facility.name), facility.id, url)
 
 
 def scrape_subcounties():
