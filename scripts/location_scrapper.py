da028e24bf385b8690b5d78f437a10d712e08b1f
Nick Hargreaves <hargreaves1234@gmail.com>
2021-09-14 20:08:33 +0300

    try free text search if no results from nearbysearch

--- scripts/location_scrapper.py
+++ scripts/location_scrapper.py
@@ -1,6 +1,4 @@
 import requests
-import pymysql.cursors
-
 from api.models import SubCounty
 
 
@@ -13,16 +11,23 @@ def scrape_subcounties():
     from api.models import County
     counties = County.objects.all()
     for county in counties:
-        subcounties = SubCounty.objects.filter(county=county)
+        subcounties = SubCounty.objects.filter(county=county, location='')
         places_search_path = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?radius=50000' \
                              '&key=' + google_api_key
         for subcounty in subcounties:
-            response = requests.get(places_search_path + "&name=" + subcounty.name + "&location=" + county.location).json()
+            url = places_search_path + "&name=" + subcounty.name + "&location=" + county.location
+            response = requests.get(url).json()
             results = response['results']
+            if len(results) < 1:
+                # try text search
+                url = "https://maps.googleapis.com/maps/api/place/textsearch/json?key=" + google_api_key \
+                      + "&query=" + subcounty.name
+                response = requests.get(url).json()
+                results = response['results']
             if len(results) > 0:
                 place = results[0]
                 location = str(place['geometry']['location']['lat']) + ',' + str(place['geometry']['location']['lng'])
                 subcounty.location = location
                 subcounty.save()
             else:
-                print("404 - ", str(subcounty.name), subcounty.id)
+                print("404 - ", county.name, str(subcounty.name), subcounty.id, url)
