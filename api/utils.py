02079f0ba1391e89a6288757c0f16fa5cc98b13a
Nick Hargreaves <hargreaves1234@gmail.com>
2021-09-14 22:33:57 +0300

    function to find closest facilities

---
+++ api/utils.py
@@ -0,0 +1,15 @@
+import collections
+import itertools
+
+
+def find_closest_facilities(origin):
+    from api.models import VaccineCenter
+    facilities = VaccineCenter.objects.filter(location__isnull=False)
+    facilities_distances = {}
+    for facility in facilities:
+        loc = facility.location.split(",")
+        distance = (float(loc[0]) - origin[0])**2 + (float(loc[1]) - origin[1])**2
+        facilities_distances.update({distance: facility.name})
+    facilities_distances = collections.OrderedDict(sorted(facilities_distances.items()))
+    closest_facilities = dict(itertools.islice(facilities_distances.items(), 10))
+    return closest_facilities
