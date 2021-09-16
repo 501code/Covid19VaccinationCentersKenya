e0073442378b555092612e30439ba4d048a52355
Nick Hargreaves <hargreaves1234@gmail.com>
2021-09-16 21:13:23 +0300

    free text search without picking from dropdown

--- landingpage/views.py
+++ landingpage/views.py
@@ -1,14 +1,12 @@
 import json
 
+import requests
 from django.conf import settings
-from django.db.models import Q
-from django.http import HttpResponse
 from django.shortcuts import render
-from django.template import loader
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 
-from api.models import VaccineCenter, News
+from api.models import News
 
 
 class LandingPage(View):
@@ -27,9 +25,22 @@ class LandingPage(View):
     @csrf_exempt
     def post(self, request):
         center = request.POST.get('center')
-        center = json.loads(center.replace(" lat", '"lat"').replace(" lng", '"lng"'))
-        location = [center['lat'], center['lng']]
-        from api.utils import find_closest_facilities
-        nearest_facilities = find_closest_facilities(location)
-        self.context.update({'facilities': nearest_facilities})
+        if center:
+            center = json.loads(center.replace(" lat", '"lat"').replace(" lng", '"lng"'))
+            location = [center['lat'], center['lng']]
+        else:
+            # try free text search
+            facility_address = request.POST.get('facility-address')
+            url = "https://maps.googleapis.com/maps/api/place/textsearch/json?key=" + settings.GOOGLE_MAPS_API_KEY \
+                  + "&query=" + facility_address
+            response = requests.get(url).json()
+            results = response['results']
+            if len(results) > 0:
+                place = results[0]
+                location = [float(place['geometry']['location']['lat']), float(place['geometry']['location']['lng'])]
+                from api.utils import find_closest_facilities
+                nearest_facilities = find_closest_facilities(location)
+                self.context.update({'facilities': nearest_facilities})
+            else:
+                self.context.update({'error_message': 'Location not found!'})
         return render(request, self.template_name, context=self.context)
